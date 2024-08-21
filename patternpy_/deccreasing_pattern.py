320e07c20ddd6804620c42588b8cfb7b4b561b14
Shadab <sadabansari451@gmail.com>
2024-08-21 16:57:54 +0530

    pattern commited

---
+++ patternpy_/deccreasing_pattern.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number:" ))
+for i in range(n):  #number of rows
+    for j in  range(i,n):
+        print("*",end=" ") #printing and  spacing
+    print() # next line
+
+
+#  0 to 5*****
+#  1 to 5****
+#  2 to 5***
+#  3 to 5**
+#  4 to 5*
---
+++ patternpy_/square_pattern.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a number:" ))
+for i in range(n):  #number of rows
+    for j in  range(n): #number of columns 
+        print("*",end=" ") #printing and  spacing
+    print() # next line
